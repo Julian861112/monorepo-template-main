6b5a8aacdfd1764e3c03614c660d78d20a6b54e1
Julian861112 <julianstory1@gmail.com>
2023-09-11 22:00:24 -0700

    Added comments and setter for class Rectangle

--- Lab1/src/rectangle.py
+++ Lab1/src/rectangle.py
@@ -6,6 +6,8 @@
  so a default one will be created for us.
 """
 
+# Abstract class Shape, which has two methods set_values and area
+
 
 class Shape:
     @abstractmethod
@@ -15,20 +17,34 @@ class Shape:
     def area(self):
         pass
 
+# Class Rectangle extends class Shape, which has two private variables width, height
+
 
 class Rectangle(Shape):
+    # Constructor for rectangle
     def set_values(self, width, height):
         self.width = width
         self.height = height
+    # Function to return the area of rectangle
 
     def area(self):
         return self.width * self.height
+    # getter for Width
 
     def getWidth(self):
         return self.width
+    # getter for height
 
     def getHeight(self):
         return self.height
+    # setter for width
+
+    def setWidth(self, width):
+        self.width = width
+    # setter for height
+
+    def setHeight(self, height):
+        self.height = height
 
 
 if __name__ == "__main__":
